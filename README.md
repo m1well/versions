f01fa5b896835e17e4208239a409b9c2b3676577
Michael Wellner <michaelwellner@gmx.de>
2017-11-30 00:39:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# versions
+
+This is a script to display the version numbers of your tools.</br>
+It is probably never complete... ;)</br>
+
+
+## Problem
+I want to see the version numbers of all my used tools with one command.</br>
+
+
+## Solution
+<pre>
+This script.</br>
+Actually following tools are listed (alphabetically):</br>
+* OS (Windows or macOS)
+* AngularCli
+* Bash
+* Docker
+* Git
+* Gradle
+* Gulp
+* Java
+* Maven
+* Npm
+* Node
+* SpringCli
+* Zsh
+</pre>
+Screenshot:</br>
+![screenshot](screen.png "Screenshot")
+
+
+## Usage
+#### versions.sh
+If you don't have any listed tool, then the script doesn't print it out.</br>
+
+## Environment Info
+##### unix
+I am not so familiar with all different unix shells, but i think u can use it with all standard shells</br>
+##### windows
+For windows i take the gitBash shell to use the script</br>
+
+
+## Contribution
+You are welcome to contribute this project! Please follow the standard rules.</br>
+If you find a bug or have any other tools to display, then please firstly open an issue.</br>
+If you are creating a Pull Request, please update the version & date of last change in the script - and use [SemVer](http://semver.org).</br>
+Also please take care to indent with 2 spaces.</br>
+Thank you.</br>
+
+
+## Copyright and License
+Copyright :copyright: 2017 Michael Wellner ([@m1well](http://www.twitter.m1well.de))</br>
+Code released under the [MIT License](/LICENSE).</br>
---
+++ versions.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+###
+#title                  :versions.sh
+#description            :This script provides your tool versions.
+#author                 :Michael Wellner (@m1well) twitter.m1well.de
+#date of creation       :20171130
+#date of last change    :20171130
+#version                :1.0.0
+#usage                  :versions.sh
+#notes                  :it would be most suitable to create an alias
+###
+
+### colors and linebreak ###
+BR="\n"
+FONT_CYAN="\033[0;96m"
+FONT_NONE="\033[0m"
+
+### output ###
+line="###############################################"
+header="########## versions (alphabetically) ##########"
+
+isThisStringVersionNumber() {
+   if [[ "${1}" =~ [0-9]+\.[0-9]+\.* ]] ; then return 0 ; fi
+   return 1
+}
+printStartLines() {
+  printf "${FONT_CYAN}"
+  printf "${line}${BR}"
+  printf "${header}${BR}"
+  printf "${line}${BR}"
+  printf "${FONT_NONE}"
+}
+printEndLines() {
+  printf "${FONT_CYAN}"
+  printf "${line}${BR}"
+  printf "${FONT_NONE}"
+}
+
+getOsVersion() {
+  local os=""
+  # check if windows
+  os="$(echo $(systeminfo 2>&1 | grep -i 'Betriebssystemname' | tr -s " " | sed 's/Betriebssystemname: //g'))"
+  if [ "${os}" != "" ] ; then
+    echo "${os}"
+  else
+    # check if macos
+		os="$(echo $(system_profiler SPSoftwareDataType 2>&1 | grep -i 'System Version:' | tr -s " " | sed 's/System Version: //g' | cut -d " " -f 2,3))"
+		if [ "${os}" != "" ] ; then
+      echo "${os}"
+		else
+      echo "-"
+		fi
+	fi
+}
+printOsVersion() {
+	local os=$(getOsVersion)
+	if [ "${os}" != "-" ] ; then
+    printf "os:            "
+    printf "${os}"
+    printf "${BR}"
+    printf "# -----------------------------------"
+    printf "${BR}"
+  fi
+}
+
+printAngularCliVersion() {
+	local version=""
+	version="$(echo $(ng -v 2>&1 | grep -i 'Angular' | cut -d " " -f 3))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "angular cli:   "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printBashVersion() {
+	local version=""
+	version="$(echo $(bash --version 2>&1 | grep -i 'bash' | cut -d " " -f 4))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "bash:          "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printDockerVersion() {
+	local version=""
+	version="$(echo $(docker -v 2>&1 | grep -i 'Docker version' | cut -d " " -f 3 | sed 's/\,/ /g'))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "docker:        "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printGitVersion() {
+	local version=""
+	version="$(echo $(git --version 2>&1 | cut -d " " -f 3))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "git:           "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printGradleVersion() {
+	local version=""
+	version="$(echo $(gradle -v 2>&1 | grep -i 'Gradle' | cut -d " " -f 2))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "gradle:        "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printGulpVersion() {
+	local version=""
+	version="$(echo $(gulp -v 2>&1 | grep -i 'CLI version' | cut -d " " -f 4))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "gulp:          "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printJavaVersion() {
+	local version=""
+	version="$(echo $(java -version 2>&1 | grep -i 'version' | cut -d " " -f 3 | sed 's/\"/ /g'))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "java:          "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printMavenVersion() {
+	local version=""
+	version="$(echo $(mvn -v 2>&1 | grep -i 'Apache Maven' | cut -d " " -f 3))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "maven:         "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printNpmVersion() {
+	local version=""
+	version="$(echo $(npm -v))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "npm:           "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printNodeVersion() {
+	local version=""
+	version="$(echo $(node -v | cut -d "v" -f 2))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "node:          "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printSpringCliVersion() {
+	local version=""
+	version="$(echo $(spring --version 2>&1 | grep -i 'CLI' | cut -d " " -f 3 | cut -d "v" -f 2))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "spring cli:    "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+printZshVersion() {
+	local version=""
+	version="$(echo $(zsh --version 2>&1 | grep -i 'zsh' | cut -d " " -f 2))"
+	if isThisStringVersionNumber "${version}" ; then
+		printf "zsh:           "
+	  printf "${version}"
+	  printf "${BR}"
+	fi
+}
+
+
+### start of script ###
+printStartLines
+printOsVersion
+printAngularCliVersion
+printBashVersion
+printDockerVersion
+printGitVersion
+printGradleVersion
+printGulpVersion
+printJavaVersion
+printMavenVersion
+printNpmVersion
+printNodeVersion
+printSpringCliVersion
+printZshVersion
+printEndLines
+
+### end of script ###
+#####################
